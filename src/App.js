05966d919e02aaa2fd9292740ca3d0aca4c0e866
Grisha Angelov <grisha.angelov89@gmail.com>
2023-09-28 09:32:47 +0200

    implement Header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Header from "components/Header"
+
 function App() {
   return (
     <div className="container-fluid">
       <div className="row vh-100 bg-primary-subtle">
         <div className="col-md-12">
-          <h1>App</h1>
+          <Header />
         </div>
       </div>
     </div>
---
+++ src/components/Header/Header.js
@@ -0,0 +1,7 @@
+const Header = () => (
+  <h1 className="text-center text-primary border border-primary shadow rounded mt-2 p-1">
+    Find all pairs or sums equal to 10
+  </h1>
+)
+
+export default Header
